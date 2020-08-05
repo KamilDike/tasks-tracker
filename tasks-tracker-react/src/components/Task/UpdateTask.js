4b80eed0fc4cbbc7d24d370555f189411234e937
Kamil Dike <kamil.dike.96@gmail.com>
2020-08-05 16:50:57 +0200

    Finished Update Task activity, final build v0.1

--- tasks-tracker-react/src/components/Task/UpdateTask.js
+++ tasks-tracker-react/src/components/Task/UpdateTask.js
@@ -1,15 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import classnames from 'classnames'
-import { getTaskAction } from '../../actions/TaskActions'
+import { getTaskAction, addTaskAction } from '../../actions/TaskActions'
 
 function UpdateTask(props) {
+    const [id, setId] = useState("")
+    const [summary, setSummary] = useState("")
+    const [acceptanceCriteria, setAcceptanceCriteria] = useState("")
+    const [status, setStatus] = useState("")
+    const [errors, setErrors] = useState({})
+
     useEffect(() => {
-        const { task_id } = props.match.params;
-        props.getTaskAction(task_id); 
+            const { task_id } = props.match.params;
+            props.getTaskAction(task_id)
     }, [])
+
+    useEffect(() => {
+        console.log('props loaded')
+        if (props.errors) {
+            console.log(props.errors)
+            setErrors(props.errors)
+        }
+
+        setId(props.task.id)
+        setSummary(props.task.summary)
+        setAcceptanceCriteria(props.task.acceptanceCriteria)
+        setStatus(props.task.status)
+    }, [props])
+
+    function onSubmit(e) {
+        e.preventDefault()
+        const updatedTask = {
+            id: id,
+            summary: summary,
+            acceptanceCriteria: acceptanceCriteria,
+            status: status
+        };
+        console.log(updatedTask)
+        props.addTaskAction(updatedTask, props.history)
+    }
 
     return (
         <div className="addProjectTask">
@@ -20,15 +51,22 @@ function UpdateTask(props) {
                             Back to Board
                         </Link>
                         <h4 className="display-4 text-center">Add /Update Project Task</h4>
-                        <form>
+                        <form onSubmit={onSubmit}>
                             <div className="form-group">
-                                <input type="text" className="form-control form-control-lg" name="summary" placeholder="Project Task summary" />
+                                <input type="text" 
+                                className={classnames("form-control form-control-lg", {
+                                    "is-invalid": errors.summary
+                                })}
+                                name="summary" placeholder="Project Task summary" value={summary}
+                                    onChange={e => setSummary(e.target.value)}/>
                             </div>
                             <div className="form-group">
-                                <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
+                                <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria" value={acceptanceCriteria}
+                                    onChange={e => setAcceptanceCriteria(e.target.value)}></textarea>
                             </div>
                             <div className="form-group">
-                                <select className="form-control form-control-lg" name="status">
+                                <select className="form-control form-control-lg" name="status" value={status}
+                                    onChange={e => setStatus(e.target.value)}>
                                     <option value="">Select Status</option>
                                     <option value="TO_DO">TO DO</option>
                                     <option value="IN_PROGRESS">IN PROGRESS</option>
@@ -47,12 +85,13 @@ function UpdateTask(props) {
 UpdateTask.propTypes = {
     task: PropTypes.object.isRequired,
     getTaskAction: PropTypes.func.isRequired,
+    addTaskAction: PropTypes.func.isRequired,
     errors: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    task: state.task,
+    task: state.tasks.task,
     errors: state.errors
 })
 
-export default connect(mapStateToProps, {getTaskAction}) (UpdateTask);
+export default connect(mapStateToProps, {getTaskAction, addTaskAction}) (UpdateTask);
